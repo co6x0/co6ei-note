56af20d3192b78b7f80b28e53e279af4d5d39c9b
Nao Komura <nao.komura@sixaxd.com>
2021-04-27 03:42:35 +0900

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import WPAPI from 'wpapi'
 import type { WP_REST_API_Posts, WP_REST_API_Post } from 'wp-types'
 
-const wp = new WPAPI({ endpoint: 'http://35.187.216.147/wp-json' })
+const wp = new WPAPI({ endpoint: String(process.env.WPAPI_ENDPOINT) })
 export const getPosts = async () => {
   const posts: WP_REST_API_Posts = await wp
     .posts()
