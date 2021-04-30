31887339e69d8e8ea7d6150a0a8c9dbe762282dd
Nao Komura <nao.komura@sixaxd.com>
2021-05-01 02:41:26 +0900

    fix lack of type

--- src/components/ArticleLink.tsx
+++ src/components/ArticleLink.tsx
@@ -2,16 +2,19 @@ import Link from 'next/link'
 
 type Props = {
   href: string
-  children: string[]
+  children: string[] | undefined
 }
 
 export const ArticleLink: React.VFC<Props> = ({ href, children }) => {
+  // console.log(children[0])
   const cmsUrl = String('https://' + process.env.NEXT_PUBLIC_CMS_DOMAIN)
   const splitBaseUrl = (href: string) => href.replace(cmsUrl, '')
 
   return href.startsWith(cmsUrl) ? (
-    <Link href={splitBaseUrl(href)}>{children[0]}</Link>
+    <Link href={splitBaseUrl(href)}>
+      {children !== undefined ? children[0] : ''}
+    </Link>
   ) : (
-    <a href={href}>{children[0]}</a>
+    <a href={href}>{children !== undefined ? children[0] : ''}</a>
   )
 }
