5f4e335a155967d70b8f64501142118b6f9650b6
Nao Komura <nao.komura@sixaxd.com>
2021-05-05 01:20:29 +0900

    update Footer, add react-svg-loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
+const path = require('path') // eslint-disable-line
+
 module.exports = {
   images: {
     domains: [process.env.NEXT_PUBLIC_CMS_DOMAIN],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      include: [path.resolve(path.resolve(), 'src/assets')],
+      use: [
+        {
+          loader: 'react-svg-loader',
+          options: {
+            svgo: {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    })
+
+    return config
   },
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,41 @@
+import Image from 'next/image'
 import styles from './style.module.scss'
+import IconGitHub from 'assets/svg/github.svg'
+import IconNote from 'assets/svg/note.svg'
+import IconTwitter from 'assets/svg/twitter.svg'
 
 export const Footer: React.VFC = () => {
   return (
     <footer className={styles.root}>
-      <small>Copyright © Nao Komura</small>
+      <div className={styles.cat}>
+        <Image src="/images/signyan.png" width="88" height="140" alt="猫" />
+      </div>
+      <div className={styles.links}>
+        <div>
+          <a
+            href="https://twitter.com/co6ei"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconTwitter title="Twitter" />
+          </a>
+          <a
+            href="https://note.com/sixa_nao"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconNote title="Note" />
+          </a>
+          <a
+            href="https://github.com/naokomura"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconGitHub title="GitHub" />
+          </a>
+        </div>
+        <small>Copyright © Nao Komura</small>
+      </div>
     </footer>
   )
 }
