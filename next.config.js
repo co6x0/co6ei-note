ebb0f79f1007182c875b64e3e50186f29d1b78f1
Nao Komura <nao.komura@sixaxd.com>
2021-05-15 14:22:25 +0900

    change svg loader settings to use webpack5

--- next.config.js
+++ next.config.js
@@ -1,29 +1,5 @@
-const path = require('path')
-
 module.exports = {
   images: {
     domains: [process.env.NEXT_PUBLIC_CMS_DOMAIN],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      include: [path.resolve(path.resolve(), 'src/assets')],
-      use: [
-        {
-          loader: 'react-svg-loader',
-          options: {
-            svgo: {
-              plugins: [
-                {
-                  removeViewBox: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    })
-
-    return config
   },
 }
