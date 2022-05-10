71399efa2114d9e23637bfdbe1f0c5f10c4a06ee
Nao Komura <nao.komura@sixaxd.com>
2022-05-10 15:42:01 +0900

    Next.js系パッケージ更新, GA Scriptの移行

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import 'styles/globals.scss'
 import styles from 'styles/app.module.scss'
 import { AppProps } from 'next/app'
@@ -7,6 +8,24 @@ import { Footer } from 'components/Footer'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={styles.root}>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-WDHKSVZ0B1"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-WDHKSVZ0B1');
+        `}
+          </Script>
+        </>
+      )}
       <Header />
       <main>
         <Component {...pageProps} />
